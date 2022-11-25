6fb62431060e2dea72629c249005d32bfa0d6508
Maulana Sodiqin <maulanasdqn@gmail.com>
2022-11-25 23:05:32 +0700

    Maulana Sodiqin | Add Vue Components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,33 +5,27 @@ import presetUno from "@unocss/preset-uno";
 import { presetWebFonts } from "unocss";
 
 // https://astro.build/config
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    Unocss({
-      presets: [
-        presetUno(),
-        presetWebFonts({
-          provider: "google",
-          fonts: {
-            sans: "Lato",
-            mono: ["Fira Code", "Fira Mono:400,700"],
-            lobster: "Lobster",
-            bold: "Lato:800",
-            lato: [
-              {
-                name: "Lato",
-                weights: ["400", "700"],
-                italic: true,
-              },
-              {
-                name: "sans-serif",
-                provider: "none",
-              },
-            ],
-          },
-        }),
-      ],
-    }),
-    react(),
-  ],
+  integrations: [Unocss({
+    presets: [presetUno(), presetWebFonts({
+      provider: "google",
+      fonts: {
+        sans: "Lato",
+        mono: ["Fira Code", "Fira Mono:400,700"],
+        lobster: "Lobster",
+        bold: "Lato:800",
+        lato: [{
+          name: "Lato",
+          weights: ["400", "700"],
+          italic: true
+        }, {
+          name: "sans-serif",
+          provider: "none"
+        }]
+      }
+    })]
+  }), react(), vue()]
 });
---
+++ src/components/Vue/Header.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const myName = ref("Maulana Sodiqin");
+</script>
+
+<template>
+  <div>{{ myName }}</div>
+</template>
