716dea457de4f8bcdd6c7c20897e88503b60e63f
Maulana Sodiqin <maulanasdqn@gmail.com>
2023-06-12 03:45:37 +0700

    improvement: add animation on hover

--- components/molecules/sidebar/index.tsx
+++ components/molecules/sidebar/index.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
 import { AiFillProject, AiFillContacts, AiFillHome, AiFillProfile } from "react-icons/ai";
 
 const _sidebar_item = [
@@ -21,30 +21,54 @@ const _sidebar_item = [
   },
 ];
 
-export const Sidebar: FC = (): ReactElement => (
-  <motion.aside
-    className="bg-[#485364] shadow-sm shadow-primary-300 rounded-lg flex-col gap-y-2 p-2 text-gray-50 flex items-center justify-center"
-    style={{
-      position: "fixed",
-      width: "4rem",
-      height: "auto",
-      right: -130,
-      bottom: "calc(50% - 6rem)",
-    }}
-    animate={{
-      x: -140,
-    }}
-  >
-    {_sidebar_item.map((item, index) => (
-      <div
-        key={index}
-        className="bg-primary-300 group relative hover:rounded-full transition-all duration-100 delay-75 rounded-lg p-2 w-full items-center"
-      >
-        {item.icon}
-        <span className="absolute hidden text-right bg-primary-300 rounded-lg bg-cover p-1 top-3 right-[72px] w-[70px] group-hover:flex justify-center items-center">
-          {item.label}
-        </span>
-      </div>
-    ))}
-  </motion.aside>
-);
+export const Sidebar: FC = (): ReactElement => {
+  const [isHover, setIsHover] = useState(false);
+  return (
+    <motion.aside
+      className="bg-[#485364] shadow-sm shadow-primary-300 rounded-lg flex-col gap-y-2 p-2 text-gray-50 flex items-center justify-center"
+      style={{
+        position: "fixed",
+        width: "4rem",
+        height: "auto",
+        right: -120,
+        bottom: "calc(50% - 6rem)",
+      }}
+      animate={{
+        x: -140,
+      }}
+    >
+      {_sidebar_item.map((item, index) => (
+        <motion.div
+          animate={{
+            borderRadius: 10,
+          }}
+          whileHover={{
+            borderRadius: 100,
+            scaleX: 1.4,
+            scaleY: 1.4,
+            marginBottom: 16,
+            marginTop: 16,
+          }}
+          transition={{
+            borderRadius: 100,
+          }}
+          key={index}
+          className="bg-primary-300 group relative p-2 w-full items-center"
+        >
+          {item.icon}
+          <motion.span
+            style={{
+              display: isHover ? "flex" : "none",
+            }}
+            animate={{
+              x: -3,
+            }}
+            className="absolute text-center bg-primary-300 rounded-lg bg-cover p-1 top-3 right-[72px] w-[70px] justify-center items-center"
+          >
+            {item.label}
+          </motion.span>
+        </motion.div>
+      ))}
+    </motion.aside>
+  );
+};
